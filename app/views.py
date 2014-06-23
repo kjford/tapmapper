2dc407a0441a33c8b15306b7006f148179d520b2
Kevin Ford <kjford@gmail.com>
2014-06-22 21:53:33 -0700

    config server

--- app/views.py
+++ app/views.py
@@ -55,3 +55,7 @@ def page_not_found(error):
 @app.errorhandler(500)
 def internal_error(error):
     return render_template('500.html'), 500
+
+
+if __name__=="__main__":
+    app.run(host='0.0.0.0',port=5000)
