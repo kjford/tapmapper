a521ce8eeff1221ba1324ef1bb22ed8479edf9d2
Kevin Ford <kjford@gmail.com>
2014-06-28 11:39:17 -0700

    Created style table. Tweak daily updated

--- dailyupdate.sh
+++ dailyupdate.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
+cd ~/tapmapper/tapmapper/
 echo 'Processing raw tweets'
 python procrawtweets.py
 echo 'Cleaning and binning raw tweets'
 python cleanandbinproctweets.py
 echo 'Recomputing TFIDF table'
 python computetfidf.py
+echo 'Creating output files'
+python getstats.py
 echo 'Done'
