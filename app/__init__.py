0dad588eb7dfc5f04c1290e9715e66cc81cd8300
Kevin Ford <kjford@gmail.com>
2018-07-15 23:09:43 -0700

    redo settings config to use SSM in prod

--- app/__init__.py
+++ app/__init__.py
@@ -4,19 +4,8 @@ from flask import Flask
 # Creates our application.
 app = Flask(__name__)
 
-# Development configuration settings
-# WARNING - these should not be used in production
-app.config.from_pyfile('settings/development.cfg')
+app.config.from_pyfile('settings/settings.py', silent=True)
 
-# Production configuration settings
-# To have these override your development settings,
-# you'll need to set your environment variable to
-# the file path:
-# export FLASK_SETTINGS=/path/to/settings.cfg
-app.config.from_envvar('FLASK_SETTINGS', silent=True)
-
-# Application DEBUG - should be True in development
-# and False in production
 app.debug = app.config["DEBUG"]
 
 # DATABASE SETTINGS
@@ -25,6 +14,5 @@ port = app.config["DATABASE_PORT"]
 user = app.config["DATABASE_USER"]
 passwd = app.config["DATABASE_PASSWORD"]
 db = app.config["DATABASE_DB"]
-
 
 from app import views
---
+++ app/settings/settings.py
@@ -0,0 +1,40 @@
+import boto3
+import os
+
+
+def get_aws_parameter(param_name, region='us-west-2'):
+    """
+    This function reads a secure parameter from AWS' SSM service.
+    The request must be passed a valid parameter name, as well as
+    temporary credentials which can be used to access the parameter.
+    The parameter's value is returned.
+    """
+    # Create the SSM Client
+    ssm = boto3.client('ssm', region_name=region)
+
+    # Get the requested parameter
+    response = ssm.get_parameter(Name=param_name, WithDecryption=True)
+
+    return response['Parameters']['Value']
+
+
+ENV = os.environ.get('FLASK_ENV', 'production')
+DATABASE_PORT = os.environ.get('DATABASE_PORT', 3306)
+DATABASE_DB = os.environ.get('DATABASE_DB', 'tapmapper')
+
+if ENV == 'production':
+    region = os.environ.get('REGION', 'us-west-2')
+    DEBUG = False
+    DATABASE_HOST = os.environ.get('DATABASE_HOST', 'localhost')
+    DATABASE_USER = os.environ.get('DATABASE_USER', 'webuser')
+    DATABASE_PASSWORD = get_aws_parameter('DATABASE_PASSWORD', region)
+
+elif ENV == 'development':
+    DEBUG = True
+    DATABASE_HOST = "db"
+    DATABASE_PORT = 3306
+    DATABASE_USER = "root"
+    DATABASE_PASSWORD = "demo"
+
+else:
+    raise ValueError("ENV must be production or development")
